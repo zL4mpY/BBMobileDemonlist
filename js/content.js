2468c473baf674f95f43bac58cacdd3bdce17277
zL4mpY <LampNotLama@yandex.ru>
2025-11-18 00:41:58 +0300

    separated the data to another git repo

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/data';
+const dir = 'https://raw.githubusercontent.com/zL4mpY/bbmdl_data/refs/heads/main';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
