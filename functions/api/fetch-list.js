500e9077922acf7a9ef19a43e962176a12fca705
zL4mpY <LampNotLama@yandex.ru>
2025-12-07 17:11:29 +0300

    test

--- functions/api/fetch-list.js
+++ functions/api/fetch-list.js
@@ -27,16 +27,17 @@
 // }
 
 export async function onRequest({ env }) {
-    const GITHUB_TOKEN = env.GITHUB_TOKEN; // берётся из секретов Cloudflare
+    // const GITHUB_TOKEN = env.GITHUB_TOKEN; // берётся из секретов Cloudflare
+    const GITHUB_TOKEN = env.GITHUB_TOKEN;
     const REPO_URL = 'https://raw.githubusercontent.com/zL4mpY/bbmdl_data/refs/heads/main';
 
     try {
         // Загружаем список уровней
         const listRes = await fetch(`${REPO_URL}/_list.json`, {
-        headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
-            Accept: 'application/vnd.github.v3.raw',
-        },
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+                Accept: 'application/vnd.github.v3.raw',
+            },
         });
 
         if (!listRes.ok) throw new Error('Failed to fetch _list.json');
